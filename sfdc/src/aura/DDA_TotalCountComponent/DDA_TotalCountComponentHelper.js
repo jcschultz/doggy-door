29381e61fe08bd2a79300c6157872de917265785
John Schultz <john@johnschultz.rocks>
2017-03-28 22:52:40 -0600

    Updating layout/presentation

--- sfdc/src/aura/DDA_TotalCountComponent/DDA_TotalCountComponentHelper.js
+++ sfdc/src/aura/DDA_TotalCountComponent/DDA_TotalCountComponentHelper.js
@@ -3,7 +3,7 @@
 	handleDataDispatch : function(component, event, helper) {
 		var records = event.getParam('records');
 		var totalCount = component.find('totalCount');
-		totalCount.set('v.value', 'Total trips through the door: ' + records.length);
+		totalCount.set('v.value', 'Total trips through the doggy door: ' + records.length);
 	},
 	
 })
