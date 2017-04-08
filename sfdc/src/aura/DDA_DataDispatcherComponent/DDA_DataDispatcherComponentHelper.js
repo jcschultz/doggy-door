1d95d611cc010b32db4fecadcc00b162d321197c
John Schultz <john@johnschultz.rocks>
2017-04-08 12:07:59 -0600

    Removing error/spinner in dispatcher

--- sfdc/src/aura/DDA_DataDispatcherComponent/DDA_DataDispatcherComponentHelper.js
+++ sfdc/src/aura/DDA_DataDispatcherComponent/DDA_DataDispatcherComponentHelper.js
@@ -1,4 +1,8 @@
 ({
+	EVENT_TYPE_SPINNER : 'SPINNER',
+	EVENT_TYPE_ERROR : 'ERROR',
+	EVENT_SHOW : 'SHOW',
+	EVENT_HIDE : 'HIDE',
 	
 	dispatchRecords : function(component, event, helper, records) {
 		var dispatchEvent = $A.get('e.c:DDA_DataDispatcherEvent');
@@ -10,13 +14,14 @@
 		dispatchEvent.fire();
 	},
 	
+	fireLayoutUpdateEvent : function(component, event, helper, eventParams) {
+		var layoutUpdateRequestEvent = $A.get('e.c:DDA_LayoutUpdateRequestEvent');
+		layoutUpdateRequestEvent.setParams(eventParams);
+		layoutUpdateRequestEvent.fire();
+	},
+	
 	getRecords : function(component, event, helper, fromTimeStamp, toTimeStamp) {
-		var spinner = component.find('spinner');
-		var notification = component.find('notification');
 		var action = component.get('c.getActivitiesByDateRange');
-		
-		$A.util.removeClass(spinner, 'slds-hide');
-		$A.util.addClass(notification, 'slds-hide');
 		
 		action.setParams({
 			'fromTimeStamp' : fromTimeStamp,
@@ -31,14 +36,16 @@
 				helper.dispatchRecords(component, event, helper, returnValue);
 			}
 			else {
-				component.set('v.error', response.getError());
-				$A.util.removeClass(notification, 'slds-hide');
+				helper.showError(component, event, helper, response.getError());
 			}
 			// console.log('response', response);
 			
-			$A.util.addClass(spinner, 'slds-hide');
+			helper.hideSpinner(component, event, helper);
 			
 		});
+		
+		helper.showSpinner(component, event, helper);
+		helper.hideError(component, event, helper);
 		
 		$A.enqueueAction(action);
 	},
@@ -48,6 +55,58 @@
 		var toTimeStamp = event.getParam('toTimeStamp');
 		
 		helper.getRecords(component, event, helper, fromTimeStamp, toTimeStamp);
+	},
+	
+	hideError : function(component, event, helper) {
+		helper.fireLayoutUpdateEvent(
+			component, 
+			event, 
+			helper, 
+			{
+				'eventType' : helper.EVENT_TYPE_ERROR,
+				'showOrHide' : helper.EVENT_HIDE,
+				'message' : ''
+			}
+		);
+	},
+	
+	hideSpinner : function(component, event, helper) {
+		helper.fireLayoutUpdateEvent(
+			component, 
+			event, 
+			helper, 
+			{
+				'eventType' : helper.EVENT_TYPE_SPINNER,
+				'showOrHide' : helper.EVENT_HIDE,
+				'message' : ''
+			}
+		);
+	},
+	
+	showError : function(component, event, helper, error) {
+		helper.fireLayoutUpdateEvent(
+			component, 
+			event, 
+			helper, 
+			{
+				'eventType' : helper.EVENT_TYPE_ERROR,
+				'showOrHide' : helper.EVENT_SHOW,
+				'message' : 'There was an error retrieving the Doggy Door Activity records. Error: ' + error
+			}
+		);
+	},
+	
+	showSpinner : function(component, event, helper) {
+		helper.fireLayoutUpdateEvent(
+			component, 
+			event, 
+			helper, 
+			{
+				'eventType' : helper.EVENT_TYPE_SPINNER,
+				'showOrHide' : helper.EVENT_SHOW,
+				'message' : ''
+			}
+		);
 	},
 	
 })
